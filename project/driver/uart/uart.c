01e57cba786ca420760a8c5b873e8bee3c2295ae
MYGuo <859298198@qq.com>
2021-05-10 09:30:41 +0800

    some repair of uart.c

--- project/driver/uart/uart.c
+++ project/driver/uart/uart.c
@@ -19,7 +19,7 @@
 * 名称： UART0_Open
 * 功能： 打开串口并返回串口设备文件描述
 * 入口参数： fd :文件描述符 port :串口号(ttyS0,ttyS1,ttyS2)
-* 出口参数： 正确返回为1，错误返回为0
+* 出口参数： 正确返回为0，错误返回为-1
 *****************************************************************/
 int uart_open(int fd, char* port)
 {
@@ -60,7 +60,7 @@ void uart_close(int fd)
 * databits 数据位 取值为 7 或者8
 * stopbits 停止位 取值为 1 或者2
 * parity 效验类型 取值为N,E,O,,S
-*出口参数： 正确返回为1，错误返回为0
+*出口参数： 正确返回为0，错误返回为-1
 *******************************************************************/
 int uart_set(int fd,int speed,int flow_ctrl,int databits,int stopbits,int parity)
 { 
@@ -200,7 +200,7 @@ int uart_init(int fd, int speed,int flow_ctrlint ,int databits,int stopbits,char
 * 入口参数： fd :文件描述符
 * rcv_buf :接收串口中数据存入rcv_buf缓冲区中
 * data_len :一帧数据的长度
-* 出口参数： 正确返回为1，错误返回为0
+* 出口参数： 正确返回为0，错误返回为-1
 *******************************************************************/
 int uart_recv(int fd, char *rcv_buf,int data_len)
 {
@@ -235,7 +235,7 @@ int uart_recv(int fd, char *rcv_buf,int data_len)
 * 入口参数： fd :文件描述符
 * send_buf :存放串口发送数据
 * data_len :一帧数据的个数
-* 出口参数： 正确返回为1，错误返回为0
+* 出口参数： 正确返回为0，错误返回为-1
 *******************************************************************/
 int uart_send(int fd, char *send_buf,int data_len)
 {
