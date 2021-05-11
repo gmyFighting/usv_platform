3158f7ecaec4d919736b015c84572eb2cb3129e0
MYGuo <859298198@qq.com>
2021-05-11 11:18:53 +0800

    add imu040

--- project/framework/fog_imu040b/imu040b.c
+++ project/framework/fog_imu040b/imu040b.c
@@ -1,5 +1,20 @@
 #include "imu040b.h"
 
+// 帧头(2)+PAYLOAD+CheckSum(2)
+static enum prot {
+    IMU040_HEADER1 = 0,
+    IMU040_HEADER2,
+    IMU040_STATE,
+    IMU040_PAYLOAD,
+    IMU040_IDX,
+    IMU040_CHECK_SUM,
+    IMU040_TAIL
+};
+
+static unsigned char _parse_state = 0;
+static unsigned char _ck_sum = 0;
+static unsigned int payload_index = 0;
+
 /* 自行者 IMU040B
  * 组成: 光纤陀螺(光纤环直径40mm)+mems加速度计
  * 零偏(10s): 0.2deg/h
@@ -29,5 +44,93 @@ typedef union {
     imu040b_nav_payload_t    imu040b_data;
     char                     buf_raw[33];
 } imu040b_nav_buf_t;
-
 #pragma pack()
+
+static imu040b_nav_buf_t imu040b_nav_buf;
+struct imu040_frame imu040_sample = {};
+
+static void _parse_char_reset(void)
+{
+    _parse_state = IMU040_HEADER1;
+    _ck_sum = 0;
+    payload_index = 0;
+}
+
+static void _checksum(const unsigned char ch)
+{
+	_ck_sum += ch;
+}
+
+void publish_msg()
+{
+    imu040_sample.state = imu040b_nav_buf.imu040b_data.state;
+    imu040_sample.pitch = (float)(imu040b_nav_buf.imu040b_data.pitch)/100.0;
+    imu040_sample.roll = (float)(imu040b_nav_buf.imu040b_data.roll)/100.0;
+    imu040_sample.yaw = (float)(imu040b_nav_buf.imu040b_data.yaw)/100.0;
+    imu040_sample.wx = (float)(imu040b_nav_buf.imu040b_data.wx)/10000.0;
+    imu040_sample.wy = (float)(imu040b_nav_buf.imu040b_data.wy)/10000.0;
+    imu040_sample.wz = (float)(imu040b_nav_buf.imu040b_data.wz)/10000.0;
+    imu040_sample.ax = (float)(imu040b_nav_buf.imu040b_data.ax)/10000.0;
+    imu040_sample.ay = (float)(imu040b_nav_buf.imu040b_data.ay)/10000.0;
+    imu040_sample.az = (float)(imu040b_nav_buf.imu040b_data.az)/10000.0;    
+}
+
+int imu040_parse_char(char ch)
+{
+    switch(_parse_state) {
+    case IMU040_HEADER1:
+        if(ch == 0x5A) {
+            _parse_state = IMU040_HEADER2;    
+        }
+    break;
+        
+    case IMU040_HEADER2:
+        if(ch == 0xA5) {
+            _parse_state = IMU040_STATE; 
+        }
+        else {
+            // _parse_state = IMU040_HEADER1; 
+            // _ck_sum = 0;
+            _parse_char_reset();
+        }
+    break;
+        
+    case IMU040_STATE:       
+        imu040b_nav_buf.buf_raw[payload_index++] = ch;
+        _parse_state = IMU040_PAYLOAD;
+    break;
+        
+    case IMU040_PAYLOAD:
+        _checksum(ch);
+        imu040b_nav_buf.buf_raw[payload_index++] = ch;
+        if(payload_index == 33) {
+            _parse_state = IMU040_IDX;
+        }
+	break;   
+        
+    case IMU040_IDX:
+        _parse_state = IMU040_CHECK_SUM;
+    break;
+    
+    case IMU040_CHECK_SUM:
+        if (_ck_sum == ch) {
+            _parse_state = IMU040_TAIL;
+        }
+        else {
+            _parse_char_reset();
+        }
+    break;
+
+    case IMU040_TAIL:
+        if(ch == 0x55) {
+            publish_msg();   
+        }
+
+        _parse_char_reset();
+    break;
+    }
+    
+    return 0;
+}
+
+
--- project/framework/include/imu040b.h
+++ project/framework/include/imu040b.h
@@ -1,5 +1,17 @@
 #ifndef _IMU040B_H_
 #define _IMU040B_H_
 
+struct imu040_frame {
+    unsigned char state;
+    float pitch;
+    float roll;
+    float yaw;
+    float wx;
+    float wy;
+    float wz;
+    float ax;
+    float ay;
+    float az;
+};
 
 #endif
