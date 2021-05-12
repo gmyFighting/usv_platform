eac543970b90ed64e7876d38e0ab7d1106e5f5b7
MYGuo <859298198@qq.com>
2021-05-12 16:43:54 +0800

    repair uart.c

--- project/driver/uart/uart.c
+++ project/driver/uart/uart.c
@@ -1,6 +1,5 @@
-/*******************************
- *linux串口驱动设置函数文件
- **********************************/
+ // linux串口驱动设置函数文件
+
 #include <stdio.h>// perror
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,12 +14,12 @@
 static const char FALSE = -1;
 static const char TRUE = 0;
 
-/*****************************************************************
-* 名称： UART_Open
-* 功能： 打开串口并返回串口设备文件描述
-* 入口参数： fd :文件描述符 port :串口号(ttyUSB0,ttymxc1,ttymxc2)
-* 出口参数： 正确返回为0，错误返回为-1
-*****************************************************************/
+/*
+ * 名称： UART_Open
+ * 功能： 打开串口并返回串口设备文件描述
+ * 入口参数： fd :文件描述符 port :串口号(ttyUSB0,ttymxc1,ttymxc2)
+ * 出口参数： 正确返回为0，错误返回为-1
+ */
 int uart_open(int fd, char* port)
 {
     int res;
@@ -57,17 +56,17 @@ void uart_close(int fd)
     close(fd);
 }
 
-/*******************************************************************
-* 名称： UART0_Set
-* 功能： 设置串口数据位，停止位和效验位
-* 入口参数： fd 串口文件描述符
-* speed 串口速度
-* flow_ctrl 数据流控制
-* databits 数据位 取值为 7 或者8
-* stopbits 停止位 取值为 1 或者2
-* parity 效验类型 取值为N,E,O,,S
-*出口参数： 正确返回为0，错误返回为-1
-*******************************************************************/
+/*
+ * 名称： UART_Set
+ * 功能： 设置串口数据位，停止位和效验位
+ * 入口参数： fd 串口文件描述符
+ * speed 串口速度
+ * flow_ctrl 数据流控制
+ * databits 数据位 取值为 7 或者8
+ * stopbits 停止位 取值为 1 或者2
+ * parity 效验类型 取值为N,E,O,,S
+ *出口参数： 正确返回为0，错误返回为-1
+ */
 int uart_set(int fd,int speed,int flow_ctrl,int databits,int stopbits,int parity)
 { 
     int i;
@@ -195,19 +194,19 @@ int uart_init(int fd, int speed,int flow_ctrlint ,int databits,int stopbits,char
     if (FALSE == uart_set(fd,speed,flow_ctrlint,databits,stopbits,parity)) {         
         return FALSE;
     } else {
-        printf("==================uart set==============\n=============115200 8 n 1================\n");
+        printf("uart set over\n");
         return TRUE;	   
     } 
 }
 
-/*******************************************************************
-* 名称： UART0_Recv
-* 功能： 接收串口数据
-* 入口参数： fd :文件描述符
-* rcv_buf :接收串口中数据存入rcv_buf缓冲区中
-* data_len :一帧数据的长度
-* 出口参数： 正确返回为0，错误返回为-1
-*******************************************************************/
+/*
+ * 名称： UART_Recv
+ * 功能： 接收串口数据
+ * 入口参数： fd :文件描述符
+ * rcv_buf :接收串口中数据存入rcv_buf缓冲区中
+ * data_len :一帧数据的长度
+ * 出口参数： 正确返回为0，错误返回为-1
+ */
 int uart_recv(int fd, char *rcv_buf,int data_len)
 {
     int len,fs_sel;
@@ -235,14 +234,14 @@ int uart_recv(int fd, char *rcv_buf,int data_len)
     }    
 }
 
-/*******************************************************************
-* 名称： UART0_Send
-* 功能： 发送数据
-* 入口参数： fd :文件描述符
-* send_buf :存放串口发送数据
-* data_len :一帧数据的个数
-* 出口参数： 正确返回为0，错误返回为-1
-*******************************************************************/
+/*
+ * 名称： UART_Send
+ * 功能： 发送数据
+ * 入口参数： fd :文件描述符
+ * send_buf :存放串口发送数据
+ * data_len :一帧数据的个数
+ * 出口参数： 正确返回为0，错误返回为-1
+ */
 int uart_send(int fd, char *send_buf,int data_len)
 {
     int ret;
